0faec06e1be3c915899aa2785ac5363abe8a7e56
Mahdi Soleimani <mahdisoleimani101@gmail.com>
2024-01-01 21:48:27 +0330

    piece_can_move function done this function checks whether a piece can move and returns possible positions it can move to

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 def valid_pos(coord):
-    return 0 <= coord <= 8
+    return 0 <= coord <= 7
 
 class Cell:
     def __init__(self, pos: tuple, isEmpty: bool, color: str):
@@ -74,6 +74,7 @@ class GameBoard:
         if piece.pieceIsSoldier():
             # Then we check moves for a Black Soldier Piece
             if piece_color == 'B':
+                black_moves = []
                 # A Black Soldier Piece can go Top-Left or Top-Right
                 new_pos_x = piece_pos_x - 1
                 new_pos_y1 = piece_pos_y - 1
@@ -85,7 +86,7 @@ class GameBoard:
                         # If the cell in Top-Left is empty, can move to it.
                         # So return True and coord of Top-Left cell
                         if self.matrix[new_pos_x][new_pos_y1].cellIsEmpty():
-                            return True, (new_pos_x, new_pos_y1)
+                            black_moves.append((new_pos_x, new_pos_y1))
                         # If the cell in Top-Left isn't empty...
                         else:
                             # Check if the piece in Top-Left is a White Piece
@@ -99,26 +100,14 @@ class GameBoard:
                                     # If Top-Left of the White Piece is empty,
                                     # return True and the coords of it.
                                     if self.matrix[new_pos_x1][new_pos_y11].cellIsEmpty():
-                                        return True, (new_pos_x1, new_pos_y11)
-                                    # If Top-Left of the White Piece isn't empty,
-                                    # can't move to it; so return False
-                                    else:
-                                        return False
-                                # If Top-Left of the White Piece is outta bounds,
-                                # can't move to it; so return False.
-                                else:
-                                    return False
-                            # If Top-Left isn't a White Piece (It is surely a Black Piece since it wasn't an empty cell),
-                            # can't move to it; so return False.
-                            else:
-                                return False
+                                        black_moves.append((new_pos_x1, new_pos_y11))
                     # If can go up, but going left is out of bounds.
                     # for a Black Soldier Piece both Top-Left and Top-Right can't be out of bounds at the same time.
-                    else:
+                    if valid_pos(new_pos_y2):
                         # If the cell in Top-Right is empty, can move to it.
                         # So return True and coord of Top-Right cell
                         if self.matrix[new_pos_x][new_pos_y2].cellIsEmpty():
-                            return True, (new_pos_x, new_pos_y2)
+                            black_moves.append((new_pos_x, new_pos_y2))
                         # If the cell in Top-Right isn't empty...
                         else:
                             # Check if the piece in Top-Right is a White Piece
@@ -132,26 +121,12 @@ class GameBoard:
                                     # If Top-Right of the White Piece is empty,
                                     # return True and the coords of it.
                                     if self.matrix[new_pos_x1][new_pos_y22].cellIsEmpty():
-                                        return True, (new_pos_x1, new_pos_y22)
-                                    # If Top-Right of the White Piece isn't empty,
-                                    # can't move to it; so return False
-                                    else:
-                                        return False
-                                # If Top-Right of the White Piece is outta bounds,
-                                # can't move to it; so return False.
-                                else:
-                                    return False
-                            # If Top-Right isn't a White Piece (It is surely a Black Piece since it wasn't an empty cell),
-                            # can't move to it; so return False.
-                            else:
-                                return False
-                # If a Black Soldier Piece can't move up, it is a King Piece.
-                # We don't deal with King Pieces here, so return False.
-                else:
-                    return False
+                                        black_moves.append((new_pos_x1, new_pos_y22))
+                return black_moves
             # If it isn't a Black Piece, it's definitely a White Soldier Piece.
             # So we check moves for a White Soldier Piece
             else:
+                white_moves = []
                 # A White Soldier Piece can go Bottom-Left or Bottom-Right
                 new_pos_x = piece_pos_x + 1
                 new_pos_y1 = piece_pos_y - 1
@@ -161,10 +136,10 @@ class GameBoard:
                     # If can go down, check if going left is in-bound.
                     if valid_pos(new_pos_y1):
                         # If the cell in Bottom-Left is empty, can move to it.
-                        # So return True and coord of Bottom-Left cell
+                        # save its coords.
                         if self.matrix[new_pos_x][new_pos_y1].cellIsEmpty():
-                            return True, (new_pos_x, new_pos_y1)
-                        # If the cell in Bottom-Left isn't empty...
+                            white_moves.append((new_pos_x, new_pos_y1))
+                        # If the cell in Top-Left isn't empty...
                         else:
                             # Check if the piece in Bottom-Left is a Black Piece
                             new_pos = (new_pos_x, new_pos_y1)
@@ -172,31 +147,19 @@ class GameBoard:
                                 # If it is a Black Piece, check Bottom-Left of it
                                 new_pos_x1 = new_pos_x + 1
                                 new_pos_y11 = new_pos_y1 - 1
-                                # If Bottom-Left of the White Piece isn't out of bounds...
+                                # If Bottom-Left of the Black Piece isn't out of bounds...
                                 if valid_pos(new_pos_x1) and valid_pos(new_pos_y11):
                                     # If Bottom-Left of the Black Piece is empty,
-                                    # return True and the coords of it.
+                                    # save its coords.
                                     if self.matrix[new_pos_x1][new_pos_y11].cellIsEmpty():
-                                        return True, (new_pos_x1, new_pos_y11)
-                                    # If Bottom-Left of the Black Piece isn't empty,
-                                    # can't move to it; so return False
-                                    else:
-                                        return False
-                                # If Bottom-Left of the White Piece is outta bounds,
-                                # can't move to it; so return False.
-                                else:
-                                    return False
-                            # If Bottom-Left isn't a Black Piece (It is surely a White Piece since it wasn't an empty cell),
-                            # can't move to it; so return False.
-                            else:
-                                return False
-                    # If can go up, but going left is out of bounds.
+                                        white_moves.append((new_pos_x1, new_pos_y11))
+                    # If can go down, but going left is out of bounds.
                     # for a White Soldier Piece both Bottom-Left and Bottom-Right can't be out of bounds at the same time.
-                    else:
+                    if valid_pos(new_pos_y2):
                         # If the cell in Bottom-Right is empty, can move to it.
-                        # So return True and coord of Bottom-Right cell
+                        # save its coords.
                         if self.matrix[new_pos_x][new_pos_y2].cellIsEmpty():
-                            return True, (new_pos_x, new_pos_y2)
+                            white_moves.append((new_pos_x, new_pos_y2))
                         # If the cell in Bottom-Right isn't empty...
                         else:
                             # Check if the piece in Bottom-Right is a Black Piece
@@ -205,28 +168,13 @@ class GameBoard:
                                 # If it is a Black Piece, check Bottom-Right of it
                                 new_pos_x1 = new_pos_x + 1
                                 new_pos_y22 = new_pos_y2 + 1
-                                # If Bottom-Right of the Black Piece isn't out of bounds...
+                                # If Top-Right of the White Piece isn't out of bounds...
                                 if valid_pos(new_pos_x1) and valid_pos(new_pos_y22):
                                     # If Bottom-Right of the Black Piece is empty,
-                                    # return True and the coords of it.
+                                    # save its coords
                                     if self.matrix[new_pos_x1][new_pos_y22].cellIsEmpty():
-                                        return True, (new_pos_x1, new_pos_y22)
-                                    # If Bottom-Right of the Black Piece isn't empty,
-                                    # can't move to it; so return False
-                                    else:
-                                        return False
-                                # If Bottom-Right of the Black Piece is outta bounds,
-                                # can't move to it; so return False.
-                                else:
-                                    return False
-                            # If Bottom-Right isn't a Black Piece (It is surely a White Piece since it wasn't an empty cell),
-                            # can't move to it; so return False.
-                            else:
-                                return False
-                # If a White Soldier Piece can't move down, it is a King Piece.
-                # We don't deal with King Pieces here, so return False.
-                else:
-                    return False
+                                        white_moves.append((new_pos_x1, new_pos_y22))
+                return white_moves
     
     def getMatrix(self):
         return self.matrix
@@ -252,4 +200,4 @@ gboard = GameBoard()
 bpieces = gboard.getWhitePieces()
 for piece in bpieces:
     if gboard.piece_can_move(bpieces[piece]):
-        print(piece, gboard.piece_can_move(bpieces[piece])[1])
+        print(piece, gboard.piece_can_move(bpieces[piece]))
