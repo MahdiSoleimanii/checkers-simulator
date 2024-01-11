58c9683bc57100ec283a35419cc2a3e836ca8c34
mahdisoleimanii <mahdisoleimani101@gmail.com>
2024-01-11 17:21:27 +0330

    fix minimax_alphabeta and minimax Checkers class takes depths as arguments new start method created

--- game_algo.py
+++ game_algo.py
@@ -3,12 +3,12 @@ import time
 from game_logic import *
 
 class Checkers:
-    def __init__(self, game_board: GameBoard):
+    def __init__(self, game_board: GameBoard, white_depth: int, black_depth: int):
         self.game_board = game_board
         self.SOLDIER_SCORE = 1
         self.KING_SCORE = 2
-        self.WHITE_DEPTH = 4
-        self.BLACK_DEPTH = 1
+        self.WHITE_DEPTH = white_depth
+        self.BLACK_DEPTH = black_depth
     
     def heuristic(self, board: GameBoard, player: str):
         eval_score = 0
@@ -26,83 +26,74 @@ class Checkers:
     def minimax(self, board: GameBoard, player: str, depth: int, isMaximizing: bool):
         if depth == 0 or board.game_ended():
             return self.heuristic(board, player)
+
+        best_value = float('-inf') if isMaximizing else float('inf')
+        player_pieces = board.get_black_pieces() if player == 'B' else board.get_white_pieces()
+        enemy_pieces = board.get_white_pieces() if player == 'B' else board.get_black_pieces()
         
-        possible_moves = {}
+        movable_pieces = {}
         if isMaximizing:
-            enemy_pieces = board.get_white_pieces().values() if player == 'B' else board.get_black_pieces().values()
             for piece in enemy_pieces:
-                possible_moves[piece] = board.piece_can_move(piece)
-            
-            max_eval = float('-inf')
-            for piece in possible_moves.keys():
-                for pos in possible_moves[piece]:
-                    board_copy = copy.deepcopy(board)
-                    piece_copy = copy.deepcopy(piece)
-                    board_copy.move(piece_copy, pos)
-                    eval = self.minimax(board_copy, player, depth - 1, False)
-                    max_eval = max(max_eval, eval)
-            return max_eval
+                movable_pieces[enemy_pieces[piece]] = board.piece_can_move(enemy_pieces[piece])
         else:
-            player_pieces = board.get_black_pieces().values() if player == 'B' else board.get_white_pieces().values()
             for piece in player_pieces:
-                possible_moves[piece] = board.piece_can_move(piece)
-            
-            min_eval = float('inf')
-            for piece in possible_moves.keys():
-                for pos in possible_moves[piece]:
-                    board_copy = copy.deepcopy(board)
-                    piece_copy = copy.deepcopy(piece)
-                    board_copy.move(piece_copy, pos)
-                    eval = self.minimax(board_copy, player, depth - 1, True)
-                    min_eval = min(min_eval, eval)
-            return min_eval
+                movable_pieces[player_pieces[piece]] = board.piece_can_move(player_pieces[piece])
+        
+        boards = []
+        for piece in movable_pieces:
+            for pos in movable_pieces[piece]:
+                board_copy = copy.deepcopy(board)
+                piece_copy = copy.deepcopy(piece)
+                board_copy.move(piece_copy, pos)
+                boards.append(board_copy)
+        
+        for board_cp in boards:
+            child_value = self.minimax(board_cp, player, depth - 1, not isMaximizing)
+            if isMaximizing:
+                best_value = max(best_value, child_value)
+            else:
+                best_value = min(best_value, child_value)
+        return best_value
         
     def minimax_alphabeta(self, board: GameBoard, player: str, depth: int, isMaximizing: bool, alpha, beta):
         if depth == 0 or board.game_ended():
             return self.heuristic(board, player)
+
+        best_value = float('-inf') if isMaximizing else float('inf')
+        player_pieces = board.get_black_pieces() if player == 'B' else board.get_white_pieces()
+        enemy_pieces = board.get_white_pieces() if player == 'B' else board.get_black_pieces()
         
+        movable_pieces = {}
         if isMaximizing:
-            possible_moves = {}
-            enemy_pieces = board.get_white_pieces() if player == 'B' else board.get_black_pieces()
             for piece in enemy_pieces:
-                possible_moves[enemy_pieces[piece]] = board.piece_can_move(enemy_pieces[piece])
-            
-            boards = []
-            for piece in possible_moves:
-                for pos in possible_moves[piece]:
-                    board_copy = copy.deepcopy(board)
-                    piece_copy = copy.deepcopy(piece)
-                    board_copy.move(piece_copy, pos)
-                    boards.append(board_copy)
-                    
-            for board_cp in boards:
-                eval = self.minimax_alphabeta(board_cp, player, depth - 1, False, alpha, beta)
-                alpha = max(alpha, eval)
+                movable_pieces[enemy_pieces[piece]] = board.piece_can_move(enemy_pieces[piece])
+        else:
+            for piece in player_pieces:
+                movable_pieces[player_pieces[piece]] = board.piece_can_move(player_pieces[piece])
+        
+        boards = []
+        for piece in movable_pieces:
+            for pos in movable_pieces[piece]:
+                board_copy = copy.deepcopy(board)
+                piece_copy = copy.deepcopy(piece)
+                board_copy.move(piece_copy, pos)
+                boards.append(board_copy)
+        
+        for board_cp in boards:
+            child_value = self.minimax_alphabeta(board_cp, player, depth - 1, not isMaximizing, alpha, beta)
+            if isMaximizing:
+                best_value = max(best_value, child_value)
+                alpha = max(alpha, child_value)
                 if beta <= alpha:
                     break
                 beta = min(beta, alpha)
-            return alpha
-        else:
-            possible_moves = {}
-            player_pieces = board.get_black_pieces() if player == 'B' else board.get_white_pieces()   
-            for piece in player_pieces:
-                possible_moves[player_pieces[piece]] = board.piece_can_move(player_pieces[piece])
-            
-            boards = []
-            for piece in possible_moves:
-                for pos in possible_moves[piece]:
-                    board_copy = copy.deepcopy(board)
-                    piece_copy = copy.deepcopy(piece)
-                    board_copy.move(piece_copy, pos)
-                    boards.append(board_copy)
-                    
-            for board_cp in boards:
-                eval = self.minimax_alphabeta(board_cp, player, depth - 1, True, alpha, beta)
-                beta = min(beta, eval)
+            else:
+                best_value = min(best_value, child_value)
+                beta = min(beta, child_value)
                 if beta <= alpha:
                     break
                 alpha = max(alpha, beta)
-            return beta
+        return best_value
     
     def beam_search(self, board: GameBoard, player: str, depth: int, isMaximizing: bool, alpha, beta, beam_width: int):
         if depth == 0 or board.game_ended():
@@ -199,21 +190,22 @@ class Checkers:
                 turn = not turn
         
         return steps
-                
+    
+    def start_sbs(self, current: GameBoard, turn: bool):
+        depth = self.WHITE_DEPTH if turn else self.BLACK_DEPTH
+        player = 'W' if turn else 'B'
+        return self.play(current, player, depth)  
 
 gboard = GameBoard()
-game = Checkers(gboard)
+game = Checkers(gboard, 7, 2)
 
 start_time = time.time()
-result = game.start(100)
+result = gboard
+turn = True
+result.print_board()
+while not result.game_ended():
+    result = game.start_sbs(result, turn)
+    result.print_board()
+    turn = not turn
 end_time = time.time()
-
-for board in result[-4:]:
-    board.print_board()
-    print('Black Pieces: ')
-    for piece in board.get_black_pieces():
-        print(piece, board.piece_can_move(board.get_black_pieces()[piece]))
-    print('White Pieces: ')
-    for piece in board.get_white_pieces():
-        print(piece, board.piece_can_move(board.get_white_pieces()[piece]))
-print(f"Runtime: {end_time - start_time:3f} seconds")
+print(f'Runtime: {round(end_time - start_time, 3)} seconds')
