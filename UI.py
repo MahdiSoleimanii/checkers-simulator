88e9ab8679779fbdd48ccaa8d15e21bf3879bbca
mahdisoleimanii <mahdisoleimani101@gmail.com>
2024-01-12 14:10:41 +0330

    fix UI - based on new game_algo

--- UI.py
+++ UI.py
@@ -3,46 +3,39 @@ import sys
 from game_logic import *
 from game_algo import *
 
-# Constants
 WIDTH, HEIGHT = 800, 800
 CELL_SIZE = WIDTH // 8
 FPS = 30
 
-# Colors
 CREAM = (139, 69, 19)
 BROWN = (255, 253, 208)
 WHITE = (255, 255, 255)
 BLACK = (32, 32, 32)
 CROWN_COLOR = (255, 215, 0)
 
-# Initialize Pygame
 pygame.init()
 
-# Set up the display
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers Game")
 
-
-# Checkers game logic class
 class CheckersUI:
     def __init__(self):
-        self.game = Checkers(GameBoard())
-        self.steps = self.game.start()
-        self.current_step = 0
+        self.gboard = GameBoard()
+        self.game = Checkers(self.gboard, 5, 3, 6)
 
     def draw_board(self):
         for i in range(8):
             for j in range(8):
-                rotated_x = j * CELL_SIZE  # Rotate x and y coordinates
-                rotated_y = (7 - i) * CELL_SIZE  # Adjust for the flipped y-axis
+                rotated_x = j * CELL_SIZE
+                rotated_y = (7 - i) * CELL_SIZE
 
                 pygame.draw.rect(screen, BROWN if (i + j) % 2 == 0 else CREAM, (rotated_x, rotated_y, CELL_SIZE, CELL_SIZE))
 
-                if (i, j) in self.game.game_board.get_black_pieces():
-                    piece = self.game.game_board.get_black_pieces()[(i, j)]
+                if (i, j) in self.gboard.get_black_pieces():
+                    piece = self.gboard.get_black_pieces()[(i, j)]
                     self.draw_piece(rotated_x, rotated_y, BLACK, piece)
-                elif (i, j) in self.game.game_board.get_white_pieces():
-                    piece = self.game.game_board.get_white_pieces()[(i, j)]
+                elif (i, j) in self.gboard.get_white_pieces():
+                    piece = self.gboard.get_white_pieces()[(i, j)]
                     self.draw_piece(rotated_x, rotated_y, WHITE, piece)
                     
     def draw_piece(self, x, y, color, piece):
@@ -64,24 +57,31 @@ class CheckersUI:
         clock = pygame.time.Clock()
         running = True
 
-        for step in self.steps:
+        screen.fill(BROWN)
+        self.draw_board()
+        
+        turn = True
+        while not self.gboard.game_ended():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+            player = 'W' if turn else 'B'
+            depth = self.game.WHITE_DEPTH if turn else self.game.BLACK_DEPTH
+            self.gboard = self.game.play(self.gboard, player, depth)
+            turn = not turn
 
             screen.fill(BROWN)
-            self.game.game_board = step  # Update the game board to the current step
             self.draw_board()
 
             pygame.display.flip()
             clock.tick(FPS)
-            pygame.time.wait(1000)  # Adjust the delay time between moves (milliseconds)
+            pygame.time.wait(30)
 
             if not running:
                 break
 
         pygame.quit()
         sys.exit()
-# Run the game
+
 checkers_ui = CheckersUI()
 checkers_ui.play()
